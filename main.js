45a4065ad64a4ebdbad17a8482eb9e803ab479ab
Garica-Alejo <garcia.alejoet36@gmail.com>
2023-03-29 11:47:49 -0300

    tercer commit-ejercicio 12

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Constantes del juego
-const COLUMNAS = 10;
-const FILAS = 10;
-const CANTIDAD_MINAS = 10;
+const COLUMNAS = 2;
+const FILAS = 2;
+const CANTIDAD_MINAS = 1;
 
 //Variables con colores para los casilleros (NO se pudieron declarar como constantes ya que  la fn color sólo está definida para el setup y el draw)
 var COLOR_CASILLERO_CON_MINA;
@@ -28,7 +28,8 @@ function setup()
   COLOR_CASILLERO_MARCADO = color("#278EF2");
 
   ponerMinaCasillero(0,0);
-  ponerMinaCasillero(4,3);
+
+  casillerosSinDescubrir= COLUMNAS * FILAS ;
 
   // Modificar/completar
 }
@@ -46,18 +47,32 @@ function draw() {
       }
       else {
         pintarCasillero(columnaPresionada, filaPresionada, COLOR_CASILLERO_SIN_MINA); 
+        descubrirCasillero (columnaPresionada, filaPresionada);
       }
-          
+      
+         
+    }
+    if (mouseButton == RIGHT) {
+      pintarCasillero(columnaPresionada, filaPresionada,COLOR_CASILLERO_MARCADO ); 
     }
 
     hizoClick = false;  //Indico que ya "procesé" el click del usuario. NO modificar
   }
+  if (ganoElJuego () ==true){
+    ganar ()
+  }
+
 }
 
 
 function ganoElJuego()
 {
-  return false;   //Esto hace que NUNCA gane el juego. Modificar/completar
+  if (casillerosSinDescubrir==CANTIDAD_MINAS){
+    return true;
+  }  
+  else {
+    return false;
+  } 
 }
 
 function ponerMinasTablero()
--- respuestas.md
+++ respuestas.md
@@ -1,5 +1,7 @@
-## Respuesta punto 6
+## Respuesta punto 6: 
+Al inicio del juego em pezamos con 25 casilleros sin cubrir, osea todos. 
 
-## Respuesta punto 8
+## Respuesta punto 8:
+La cantidad de casilleros sin descubrir debe ser igual a la cantidad de minas que tiene el tablero
 
 ## Respuesta punto ...
